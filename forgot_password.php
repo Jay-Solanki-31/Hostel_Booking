1872debd17db6f55afa1bd3b8c8b37484cc394d3
Jay Solanki <jaysolanki1733@gmail.com>
2024-03-13 17:05:33 +0530

    forgot password

--- forgot_password.php
+++ forgot_password.php
@@ -1,29 +1,27 @@
-<?php 
+<?php
 
 include "main_header.php";
-include "config/UserController.php";
-$UserController = new UserController();
 
 ?>
-        <!-- ACCOUNT -->
-        <section class="section-account parallax bg-11">
-            <div class="awe-overlay"></div>
-            <div class="container">
-                <div class="login-register">
-                    <div class="text text-center">
-                        <h2>Forgot Password</h2>
-            
-                     <form action="send-password-reset.php" method="post" id="" class="account_form">
-                            <div class="field-form">
-                                <input type="text" class="field-text" id=email name="email" placeholder="Email">
-                            </div>
-                            <div class="field-form field-submit">
-                                <button class="awe-btn awe-btn-13">Change Password</button>
-                            </div>
-                        </form>
+<!-- ACCOUNT -->
+<section class="section-account parallax bg-11">
+    <div class="awe-overlay"></div>
+    <div class="container">
+        <div class="login-register">
+            <div class="text text-center">
+                <h2>Forgot Password</h2>
+
+                <form action="" method="post" id="" class="account_form">
+                    <div class="field-form">
+                        <input type="text" class="field-text" id=email name="email" placeholder="Email">
                     </div>
-                </div>
+                    <div class="field-form field-submit">
+                        <button class="awe-btn awe-btn-13">Change Password</button>
+                    </div>
+                </form>
             </div>
-        </section>
-        <!-- END / ACCOUNT -->
+        </div>
+    </div>
+</section>
+<!-- END / ACCOUNT -->
 <?php include "main_footer.php" ?>
