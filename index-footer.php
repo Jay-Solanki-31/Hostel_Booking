4b63eb6ecb5e05f7c4f0014b6dc723e2eeb95819
Jay Solanki <jaysolanki1733@gmail.com>
2024-03-27 11:52:11 +0530

    chnage the footer

--- index-footer.php
+++ index-footer.php
@@ -1,73 +1,84 @@
   <!-- FOOTER -->
   <footer id="footer" class="footer-style-2">
-            <!-- FOOTER CENTER -->
-            <div class="footer_center bgr-footer">
-                <div class="container">
-                    <div class="content pt10 pb60">
-                        <div class="row">
-                            <div class="col-xs-6 col-sm-3">
-                                <div class="footer-logo clearfix">
-                                    <div class="widget widget_logo clearfix">
-                                        <div class="widget-logo text-center">
-                                            <div class="img">
-                                                <a href="#"><img class="img-center" src="images/home-3/footer/logo-footer.png" alt=""></a>
-                                            </div>
+ <!-- FOOTER CENTER -->
+ <div class="footer_center bgr-footer">
+            <div class="container">
+                <div class="content pt10 pb60">
+                    <div class="row">
+                        <div class="col-xs-6 col-sm-3">
+                            <div class="footer-logo clearfix">
+                                <div class="widget widget_logo clearfix">
+                                    <div class="widget-logo text-center">
+                                        <div class="img">
+                                            <a href="#"><img class="img-center" src="images/home-3/footer/logo-footer.png" alt=""></a>
                                         </div>
                                     </div>
-                                   
                                 </div>
-                            </div>
-                            <div class="col-xs-6 col-sm-3">
-                                <div class="widget">
-                                    <h4 class="widget-title f20 bold">Information</h4>
-                                    <div class="text">
-                                        <p><i class="lotus-icon-location mr10"></i> Anand ,Gujarat </p>
-                                        <p><i class="lotus-icon-phone mr10"></i> 2222555000</p>
-                                        </p>
-                                    </div>
-                                </div>
-                                
-                            </div>
                             
+                            </div>
+                        </div>
+                        <div class="col-xs-6 col-sm-3">
+                            <div class="widget">
+                                <h4 class="widget-title f20 bold">Information</h4>
+                                <div class="text">
+                                    <p><i class="lotus-icon-location mr10"></i> 225 Beach Street, India </p>
+                                    <p><i class="lotus-icon-phone mr10"></i>11-22-004455</p>
+                                    </p>
+                                </div>
+                            </div>
                             
                         </div>
-                    </div>
-                </div>
-                <div class="copyright">
-                    <div class="container">
-                        <p class="left inline-block f14">&copy; 2024 All rights reserved.</p>
-                        
+                        <div class="col-xs-9 col-sm-3">
+                            <div class="widget">
+                                <h4 class="widget-title f20 bold">Hotel</h4>
+                                <ul>
+                                    <li><a href="index.php">Home </a></li>
+                                    <li><a href="about.php">About Us</a></li>
+                                    <li><a href="hostels.php">Hostels</a></li>
+                                    <li><a href="contact.php">Contact</a></li>
+                                </ul>
+                            </div>
+                        </div>
+                   
                     </div>
                 </div>
             </div>
-            <!-- END / FOOTER CENTER -->
+            <div class="copyright">
+                <div class="container">
+                    <p class="left inline-block f14">&copy; 2023 All rights reserved.</p>
+                    <div class="payment pull-right inline-block">
+                        <img src="images/home-3/footer/card.png" alt="" class="img-responsive">
+                    </div>
+                </div>
+            </div>
+        </div>
+        <!-- END / FOOTER CENTER -->
 
 
-        </footer>
-        <!-- END / FOOTER -->
-    </div>
+  </footer>
+  <!-- END / FOOTER -->
 
 
-<!-- LOAD JQUERY -->
-<script data-cfasync="false" src="../../../cdn-cgi/scripts/5c5dd728/cloudflare-static/email-decode.min.js"></script><script type="text/javascript" src="js/lib/jquery-1.11.0.min.js"></script>
-<script type="text/javascript" src="js/lib/jquery-ui.min.js"></script>
-<script type="text/javascript" src="js/lib/bootstrap.min.js"></script>
-<script type="text/javascript" src="js/lib/bootstrap-select.js"></script>
-<script type="text/javascript" src="js/lib/jquery.themepunch.revolution.min.js"></script>
-<script type="text/javascript" src="js/lib/jquery.themepunch.tools.min.js"></script>
-<script type="text/javascript" src="js/lib/owl.carousel.js"></script>
-<script type="text/javascript" src="js/lib/jquery.appear.min.js"></script>
-<script type="text/javascript" src="js/lib/jquery.countTo.js"></script>
-<script type="text/javascript" src="js/lib/jquery.countdown.min.js"></script>
-<script type="text/javascript" src="js/lib/jquery.parallax-1.1.3.js"></script>
-<script type="text/javascript" src="js/lib/jquery.magnific-popup.min.js"></script>
-<script type="text/javascript" src="js/lib/SmoothScroll.js"></script>
-<!-- validate -->
-<script type="text/javascript" src="js/lib/jquery.form.min.js"></script>
-<script type="text/javascript" src="js/lib/jquery.validate.min.js"></script>
-<!-- Custom jQuery -->
-<script type="text/javascript" src="js/scripts.js"></script>
-<script type="text/javascript" src="js/customUser.js"></script>
-</body>
 
-
+  <!-- LOAD JQUERY -->
+  <script data-cfasync="false" src="../../../cdn-cgi/scripts/5c5dd728/cloudflare-static/email-decode.min.js"></script>
+  <script type="text/javascript" src="js/lib/jquery-1.11.0.min.js"></script>
+  <script type="text/javascript" src="js/lib/jquery-ui.min.js"></script>
+  <script type="text/javascript" src="js/lib/bootstrap.min.js"></script>
+  <script type="text/javascript" src="js/lib/bootstrap-select.js"></script>
+  <script type="text/javascript" src="js/lib/jquery.themepunch.revolution.min.js"></script>
+  <script type="text/javascript" src="js/lib/jquery.themepunch.tools.min.js"></script>
+  <script type="text/javascript" src="js/lib/owl.carousel.js"></script>
+  <script type="text/javascript" src="js/lib/jquery.appear.min.js"></script>
+  <script type="text/javascript" src="js/lib/jquery.countTo.js"></script>
+  <script type="text/javascript" src="js/lib/jquery.countdown.min.js"></script>
+  <script type="text/javascript" src="js/lib/jquery.parallax-1.1.3.js"></script>
+  <script type="text/javascript" src="js/lib/jquery.magnific-popup.min.js"></script>
+  <script type="text/javascript" src="js/lib/SmoothScroll.js"></script>
+  <!-- validate -->
+  <script type="text/javascript" src="js/lib/jquery.form.min.js"></script>
+  <script type="text/javascript" src="js/lib/jquery.validate.min.js"></script>
+  <!-- Custom jQuery -->
+  <script type="text/javascript" src="js/scripts.js"></script>
+  <script type="text/javascript" src="js/customUser.js"></script>
+  </body>
